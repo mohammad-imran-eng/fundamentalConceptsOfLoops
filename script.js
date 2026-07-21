22eb422ae901017135f513aafe41d158130431f6
Mohammad Imran <immu.engineer@gmail.com>
2026-07-21 18:38:51 +0600

    Write a program that: Loops from 1 to 100. Skips (continue) any number divisible by 5. Stops (break) completely once it reaches a number greater than 40. Prints every number that passes both checks.

--- script.js
+++ script.js
@@ -42,8 +42,69 @@
 
 // multiplication table using for loops
 
-for(let i = 1;i<=10;i++){
-    console.log("7", " X ", i, "=" ,7*i)
+// for(let i = 1;i<=10;i++){
+//     console.log("7", " X ", i, "=" ,7*i)
+// }
+
+
+// for(let i = 20;i>=1;i--){
+//     console.log(i);
+// }
+
+
+// let count = 0;
+// for(let i = 1; i<=50;i++){   
+    
+//     if(i%2 === 0){
+//         count = count + i;
+//     }
+// }
+
+// console.log(count);
+
+// for(let i= 1; i<=50;i++){
+    
+//     if(i === 30){
+//         console.log(i);
+//         break;
+//     }
+    
+// }
+
+// Write a for loop from 1 to 20 that skips (continue) any number divisible by 4.
+
+// for(let i = 1; i<=20;i++){
+//     if(i === 4){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// for(let i = 1; i<=5;i++){
+//     console.log(i);
+// }
+
+// let i = 1;
+// while(i<=5){
+//     console.log(i)
+//     i++
+// }
+
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++
+// }
+// while(i<5){
+//     console.log(i);
+// }
+
+for(let i = 1; i<=100;i++){
+    if(1 > 40){
+        break;
+    }
+    if(i%5 === 0){
+        continue;
+    }
+    console.log(i);
 }
-
-
