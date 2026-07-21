0521baf8cfb6416dd241ea90eacc282c014fc3e3
Mohammad Imran <immu.engineer@gmail.com>
2026-07-21 16:39:59 +0600

    some of number 1 to 10 using loop

--- script.js
+++ script.js
@@ -24,8 +24,19 @@
 
 // Write a for loop that prints all even numbers between 1 and 30.
 
-for(let i =1; i<=30;i++){
-    if(i%2 === 0){
-        console.log("Event number"+ i);
-    }
+// for(let i =1; i<=30;i++){
+//     if(i%2 === 0){
+//         console.log("Event number"+ i);
+//     }
+// }
+
+
+
+let count = 0;
+for(let i = 1;i<=20;i++){
+     count = count + i;
 }
+
+console.log(count);
+
+
