ae138e22e4239c104e826364c12a274972ec2703
Mohammad Imran <immu.engineer@gmail.com>
2026-07-21 16:31:08 +0600

    white a for loop thats prints all even numbers between 1 and 30

--- script.js
+++ script.js
@@ -22,3 +22,10 @@
 //     count++
 // }
 
+// Write a for loop that prints all even numbers between 1 and 30.
+
+for(let i =1; i<=30;i++){
+    if(i%2 === 0){
+        console.log("Event number"+ i);
+    }
+}
