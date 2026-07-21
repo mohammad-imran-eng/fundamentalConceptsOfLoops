86e08a525fa3dace16507ea1d742d9f00db9e7c5
Mohammad Imran <immu.engineer@gmail.com>
2026-07-21 16:46:32 +0600

    multiplication table with for loops

--- script.js
+++ script.js
@@ -32,11 +32,18 @@
 
 
 
-let count = 0;
-for(let i = 1;i<=20;i++){
-     count = count + i;
-}
+// let count = 0;
+// for(let i = 1;i<=20;i++){
+//      count = count + i;
+// }
 
-console.log(count);
+// console.log(count);
+
+
+// multiplication table using for loops
+
+for(let i = 1;i<=10;i++){
+    console.log("7", " X ", i, "=" ,7*i)
+}
 
 
