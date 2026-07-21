e7465ed8e5657f8dd1c36bafa83c0e8105a70594
Mohammad Imran <immu.engineer@gmail.com>
2026-07-21 16:28:00 +0600

    countdown with while loop

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-for(let i=1;i<=5;i++){
-    console.log("Hello");
-}
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Hello");
+// }
+
+// print numbers 1 to 100 using for loop
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+
+// write a while loop that's print number from 10 down to 1
+
+// let count = 10;
+// while(count>=0){
+//     console.log(count);
+//     count--
+// }
+
+// let count = 1;
+// while(count<=7){
+//     console.log("Loop চলছে");
+//     count++
+// }
 
