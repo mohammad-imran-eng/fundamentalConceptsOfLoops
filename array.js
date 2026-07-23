c5d613246f224cbc3394a114e75d4db2e0a77865
Mohammad Imran <immu.engineer@gmail.com>
2026-07-23 07:00:46 +0600

    some coding problem solve with js

---
+++ array.js
@@ -0,0 +1,189 @@
+// Adding two array in js
+
+// const numbers = [12,98,56,78,70,320,'jani na'];
+// const numbers2 = [1,2,3,4,5];
+
+// const concatArray = numbers.concat(numbers2);
+
+// console.log(concatArray);
+
+// const numbs = [2,3,4,6,7,8,9];
+// const joined = numbs.join(" / ")
+
+// console.log(joined);
+
+
+// const numbs = 30
+// console.log(Array.isArray(numbs));
+
+
+// const numbers = [2,3,4,5,6,7,8,9,10,"Arobi"];
+
+// for(const number of numbers){
+//     console.log(number)
+// }
+
+
+// const numbers = [2,3,4,5,6,7,8,9,10,"Arobi"];
+
+// for(let i = 0;i < numbers.length;i++){
+//     console.log(numbers[i])
+// }
+
+
+// const numbers = [2,3,4,5,6,7,8,9,10,"Arobi"];
+
+// const reverceArray = numbers.reverse();
+// console.log(reverceArray);
+
+// const slice = [1,2,3,4,5,6,7,8];
+// const sliceTotal = slice.slice(0,3);
+// console.log(slice);
+// console.log(sliceTotal);
+
+
+// const splice = [1,2,3,4,5,6,7,8];
+// const spliceTotal = splice.splice(0,3,100);
+// console.log(splice);
+// spliceTotal;
+
+
+//  const numbers = [2,3,4,5,6,7,8,9,10];
+//  const shortedData = numbers.sort();
+//  console.log(numbers);
+
+//  const myfav = ['arobi','imran','labib'];
+
+//  console.log(myfav)
+
+
+//  const myfav = ['arobi','imran','labib'];
+
+//  const myfavtotal = myfav.length;
+//  console.log(myfavtotal)
+
+//  const myfav = ['arobi','imran','labib','fardus,rifat'];
+
+//  console.log(myfav[0]);
+//  console.log(myfav[2])
+
+
+// const numbers = [2,3,4,5,6,7,8,9,10];
+// const nums = numbers.splice(1,1,100);
+// console.log(nums);
+// console.log(numbers);
+
+
+// const myNumber = [2,3,4,5,6,7,8];
+// const changableNumber = myNumber.push(2);
+// console.log(myNumber);
+
+// const mynums = [2,3,4,5,6,7,8,9];
+// const total = mynums.pop();
+// console.log(mynums);
+// console.log(total);
+
+// const mynums = [2,3,4,5,6,7,8,9];
+// const toatl = mynums.shift();
+// console.log(mynums);
+// console.log(toatl);
+
+
+// const mynums = [2,3,4,5,6,7,8,9,'arobi'];
+// const check = mynums.includes('arobi');
+// console.log(check);
+
+// const mynums = [2,3,4,5,6,7,8,9,'arobi'];
+
+// const position = mynums.indexOf(3);
+// console.log(position);
+
+// const mynums = [2,3,4,5,6,7,8,9];
+
+// for(let i = 0; i< mynums.length;i++){
+//     console.log(mynums[i])
+// }
+
+// const mynums = [2,3,4,5,6,7,8,9];
+// let values = 0;
+// while(values < mynums.length){
+    
+//     console.log(mynums[values]);
+//     values++
+// }
+
+
+
+// const numbers = [1,2,3,4,5];
+// // const concatArray = names.concat(numbers);
+// // console.log(concatArray);
+
+// const original = numbers.slice(0,3);
+// console.log(original);
+// console.log(numbers);
+
+// const names = ['Arobi',"Imran","Labib"];
+// const joined = names.join(" ");
+// console.log(joined);
+
+// const array1 = [1,2,3,4,5,6,7,8];
+// const variable = 20;
+// console.log(Array.isArray(array1));
+// console.log(Array.isArray(variable));
+
+// const array1 = [10,20,30,40,50];
+
+// const afterReverse = [];
+// for(data of array1){
+//     afterReverse.unshift(data);
+//     console.log(afterReverse);
+// }
+
+// const arrayReverse = array1.reverse();
+// console.log(arrayReverse);
+
+// const friends = ['abul','sabul','kabul','rabul'];
+
+// const arrayShort = friends.sort();
+
+// console.log(arrayShort);
+
+
+// const numbers = [1,2,3,4,5,6,7,8,100,85,56];
+
+// // const shortedArray = numbers.sort((a,b)=> a-b);
+// // console.log(shortedArray);
+
+// for(number of numbers){
+//     if(number > 10){
+//         console.log(number);
+//     }
+// }
+
+// const numbers = [10,30,45,45,67,89,90,67];
+
+// const unique = [];
+
+// for(let i = 0; i < numbers.length;i++){
+//     if(!unique.includes(numbers[i])){
+//         unique.push(numbers[i])
+//     }
+// }
+
+// console.log(unique);
+
+// const mixedArray = [10,"Imran",20,"Arobi",30,"Labib"];
+
+// const stringArray = [];
+// const numberArray = [];
+
+// for(data of mixedArray){
+//     if(typeof data === "string"){
+//         stringArray.push(data);
+//     }
+//     else if(typeof data === 'number'){
+//         numberArray.push(data)
+//     }
+// }
+
+// console.log(stringArray,numberArray);
